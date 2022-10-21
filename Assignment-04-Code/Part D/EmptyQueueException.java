11b45086aca0af9131738e91f96c186bc5919434
tomyumyum <nunapanda2013@gmail.com>
2022-10-20 17:47:38 -0700

    i think this is done check again

--- Assignment-04-Code/Part D/EmptyQueueException.java
+++ Assignment-04-Code/Part D/EmptyQueueException.java
@@ -1,1 +1,14 @@
-package assignment04PartD;/** * * Part D * */public class EmptyQueueException extends RuntimeException{	} // end EmptyQueueException
+package assignment04PartD;
+
+/**
+ *
+ * Part D
+ *
+ */
+
+public class EmptyQueueException extends RuntimeException {
+    public EmptyQueueException(String s){
+        super(s);
+    }
+	
+} // end EmptyQueueException
